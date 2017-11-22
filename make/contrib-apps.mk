a935cce2490fcb77b39ae7ccfc2e696b6fd303d9
obi <_obiwan_@web.de>
2017-11-22 11:28:05 +0100

    add lost busybox patches

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -7,6 +7,8 @@ BUSYBOX_PATCH  = busybox-$(BUSYBOX_VER)-nandwrite.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-unicode.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-extra.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-flashcp-small-output.patch
+BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-mount_single_uuid.patch
+BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-xinsmod-xtar-xdev.patch
 
 $(ARCHIVE)/$(BUSYBOX_SOURCE):
 	$(WGET) https://busybox.net/downloads/$(BUSYBOX_SOURCE)
